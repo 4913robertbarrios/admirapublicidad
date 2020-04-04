739e66d04d17ae2d3025b54a01509b697f45968f
admirapublicidad <55028081+4913robertbarrios@users.noreply.github.com>
2020-04-04 16:50:41 -0300

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,7 +1,7 @@
 <?php
  $destino="admirapublicidad.info@gmail.com";
  $nombre=$_POST["nombre"];
- $correo=$_POST["correo"];;
+ $correo=$_POST["correo"];
  $mensaje=$_POST["mensaje"];
  $contenido="Nombre: ".$nombre."\nCorreo: ".$correo."\nMensaje: ".$mensaje;
  mail($destino,"Contact", $contenido);
